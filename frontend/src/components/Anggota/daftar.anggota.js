8cc744f74d792848d3ae2a4287dcf0549fd42e3b
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-22 02:30:49 +0700

    frontend: daftar anggota action

--- frontend/src/components/Anggota/daftar.anggota.js
+++ frontend/src/components/Anggota/daftar.anggota.js
@@ -54,6 +54,9 @@ const DataAnggota = () => {
                 <th scope="col" className="py-3 px-6">
                   NO TELEPON
                 </th>
+                <th scope="col" className="py-3 px-6">
+                  AKSI
+                </th>
               </tr>
             </thead>
             <tbody>
@@ -71,6 +74,20 @@ const DataAnggota = () => {
                       <td className="py-4 px-6">{anggota.jenis_kelamin}</td>
                       <td className="py-4 px-6">{anggota.alamat}</td>
                       <td className="py-4 px-6">{anggota.no_telepon}</td>
+                      <td className="py-4 px-6">
+                        <button
+                          type="button"
+                          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                          Ubah
+                        </button>
+                        <button
+                          type="button"
+                          class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                        >
+                          Hapus
+                        </button>
+                      </td>
                     </tr>
                   </React.Fragment>
                 );
