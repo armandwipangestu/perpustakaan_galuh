a51922887eae8af9748160d235b05fdb3ccac0a5
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-21 12:05:11 +0700

    frontend: stuff

--- frontend/src/components/Home/main.home.js
+++ frontend/src/components/Home/main.home.js
@@ -1,5 +1,12 @@
+import React from "react";
+import ListBooks from "../Books/list.books";
+
 const Home = () => {
-  return "ini adalah Home";
+  return (
+    <React.Fragment>
+      <ListBooks />
+    </React.Fragment>
+  );
 };
 
 export default Home;
