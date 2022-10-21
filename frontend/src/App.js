fec76da6406ca3114a37bc95aef706e0be40899b
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-21 14:58:00 +0700

    frontend: loan book

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Navigation from "./components/Layouts/navigation.layouts";
 import Footer from "./components/Layouts/footer.layouts";
 import Home from "./components/Home/main.home";
 import AddBook from "./components/Books/add.books";
+import LoanBook from "./components/Books/loan.books";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/tambah_buku" element={<AddBook />} />
+          <Route path="/pinjam_buku" element={<LoanBook />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- frontend/src/components/Books/list.books.js
+++ frontend/src/components/Books/list.books.js
@@ -27,9 +27,17 @@ const ListBooks = () => {
           <Link to={`/tambah_buku`}>
             <button
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="md:mr-5 mb-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Tambahkan Buku Baru
+            </button>
+          </Link>
+          <Link to={`/pinjam_buku`}>
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Buat Pinjaman Buku
             </button>
           </Link>
         </div>
