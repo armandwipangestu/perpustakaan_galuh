1b00d8ae227ae3fd359ff20806977174a508a150
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-22 08:25:33 +0700

    frontend: daftar pengarang fix action, search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,22 @@ import MainLayouts from "./components/Layouts/main.layouts";
 import Navigation from "./components/Layouts/navigation.layouts";
 import Footer from "./components/Layouts/footer.layouts";
 import Home from "./components/Home/main.home";
+
+// Anggota
 import DataAnggota from "./components/Anggota/daftar.anggota";
 import TambahAnggota from "./components/Anggota/tambah2.anggota";
 import EditAnggota from "./components/Anggota/edit.anggota";
+
+// Pengarang
 import DataPengarang from "./components/Master_Buku/pengarang/daftar.pengarang";
 import TambahPengarang from "./components/Master_Buku/pengarang/tambah.pengarang";
+import EditPengarang from "./components/Master_Buku/pengarang/edit.pengarang";
+
+// Penerbit
 import DataPenerbit from "./components/Master_Buku/penerbit/daftar.penerbit";
 import TambahPenerbit from "./components/Master_Buku/penerbit/tambah.penerbit";
+
+// Buku
 import DataBuku from "./components/Master_Buku/buku/daftar.buku";
 import TambahBuku from "./components/Master_Buku/buku/tambah.buku";
 // import AddBook from "./components/Books/add.books";
@@ -26,13 +35,22 @@ const App = () => {
           <Route path="/" element={<Home />} />
           {/* <Route path="/tambah_buku" element={<AddBook />} /> */}
           <Route path="/pinjam_buku" element={<LoanBook />} />
+
+          {/* Anggota */}
           <Route path="/data_anggota" element={<DataAnggota />} />
           <Route path="/tambah_anggota" element={<TambahAnggota />} />
           <Route path="/edit_anggota/:id" element={<EditAnggota />} />
+
+          {/* Pengarang */}
           <Route path="/data_pengarang" element={<DataPengarang />} />
           <Route path="/tambah_pengarang" element={<TambahPengarang />} />
+          <Route path="/edit_pengarang/:id" element={<EditPengarang />} />
+
+          {/* Penerbit */}
           <Route path="/data_penerbit" element={<DataPenerbit />} />
           <Route path="/tambah_penerbit" element={<TambahPenerbit />} />
+
+          {/* Buku */}
           <Route path="/data_buku" element={<DataBuku />} />
           <Route path="/tambah_buku" element={<TambahBuku />} />
         </Routes>
