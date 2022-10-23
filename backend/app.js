3d4570b3684e399c16a90b38927f98e7af631325
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-23 05:44:57 +0000

    api with ssl

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,21 @@ import PenerbitRoute from "./routes/penerbit.routes.js";
 import PeminjamRoute from "./routes/peminjam.routes.js";
 import PengembalianBuku from "./routes/pengembalian.routes.js";
 
+import * as http from "http";
+import * as https from "https";
+import * as fs from 'node:fs';
+const privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+};
+
+const options = {
+	key: '',
+	cert: '',
+};
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -18,6 +33,11 @@ app.use(PenerbitRoute);
 app.use(PeminjamRoute);
 app.use(PengembalianBuku);
 
-app.listen(5000, () => {
-  console.log("Server up and running on localhost:5000");
-});
+//app.listen(5000, () => {
+//  console.log("Server up and running on localhost:5000");
+//});
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(4900);
+httpsServer.listen(5000);
