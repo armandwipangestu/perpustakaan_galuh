9b70a8358aa6e037e4f1c031860c6aad43c41a01
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-23 09:17:56 +0700

    backend

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -1,13 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const db_name = "perpustakaan_galuh";
-const db_host = "localhost";
-const db_user = "root";
-const db_pass = "roottoor";
+const db_name = "armandwi_perpustakaan_galuh";
+const db_host = "armandwipangestu.my.id";
+const db_user = "armandwi_perpustakaan_galuh";
+const db_pass = "O$gWqiR[f-ew";
 const db_vendor = "mysql";
 
 const DatabaseConnection = new Sequelize(db_name, db_user, db_pass, {
   host: db_host,
+  port: 3306,
   dialect: db_vendor,
 });
 
